51393cac5f2e54fd363c2b3ae926b67523da6119
Abdul Rehman Yasir <yasirabdulrehman20@gmail.com>
2026-09-22 11:43:50 +0500

    Debug Vercel request body

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
-const server = require("../server");
+module.exports = async (req, res) => {
+  console.log("METHOD:", req.method);
+  console.log("BODY:", req.body);
+  console.log("HEADERS:", req.headers);
 
-module.exports = (req, res) => {
-  return server.emit("request", req, res);
+  res.statusCode = 200;
+  res.end(JSON.stringify({
+    method: req.method,
+    body: req.body || null,
+    headers: req.headers
+  }));
+};module.exports = async (req, res) => {
+  console.log("METHOD:", req.method);
+  console.log("BODY:", req.body);
+  console.log("HEADERS:", req.headers);
+
+  res.statusCode = 200;
+  res.end(JSON.stringify({
+    method: req.method,
+    body: req.body || null,
+    headers: req.headers
+  }));
 };
